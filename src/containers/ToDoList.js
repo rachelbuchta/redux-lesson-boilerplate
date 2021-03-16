89c40fd7fcc2117c71d30f5407408ef310749d1b
rachelbuchta <>
2021-03-16 14:07:42 -0600

    Map state and dispatch in todolist

--- src/containers/ToDoList.js
+++ src/containers/ToDoList.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { showAll } from '../actions'
+import { showAll, showActive, showCompleted } from '../actions'
 import ToDo from '../containers/ToDo'
 
-const ToDoList = ({todos, showAll}) => {
+const ToDoList = ({todos, showAll, showActive, showCompleted}) => {
   const displayTodos = todos.map(todo => {
     return (
       <ToDo 
@@ -17,8 +17,8 @@ const ToDoList = ({todos, showAll}) => {
     <>
       <section>
         <button onClick={()=>showAll()}>Show All</button>
-        <button>Show Active</button>
-        <button>Show Completed</button>
+        <button onClick={()=>showActive()}>Show Active</button>
+        <button onClick={()=>showCompleted()}>Show Completed</button>
       </section>
       <ul>
         {displayTodos}
@@ -28,11 +28,16 @@ const ToDoList = ({todos, showAll}) => {
 }
 
 const mapStateToProps = state => ({
-  todos: state.todos.todos
+  todos: state.todos.todos,
+  active: state.todos.active,
+  completed: state.todos.completed,
+  clicked: state.todos.clicked
 })
 
 const mapDispatchToProps = dispatch => ({
-  showAll: () => dispatch( showAll() )
+  showAll: () => dispatch( showAll() ),
+  showActive: () => dispatch( showActive() ),
+  showCompleted: () => dispatch( showCompleted() )
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(ToDoList)
