87f53cec7ff3389388ca61962f1149026b34cb95
rachelbuchta <>
2021-03-16 15:06:08 -0600

    Add functionality to remove todo

--- src/containers/ToDo.js
+++ src/containers/ToDo.js
@@ -1,21 +1,25 @@
 import React from 'react'
-import { toggleToDo } from '../actions'
+import { toggleToDo, removeToDo } from '../actions'
 import { connect } from 'react-redux'
 
-const ToDo = ({ id, text, completed, toggleToDo }) => {
+const ToDo = ({ id, text, completed, toggleToDo, removeToDo }) => {
 
   return (
+    <>
+    <button onClick={() => removeToDo(id)}>X</button>
     <li 
       onClick={()=>toggleToDo(id)}
       className={completed ? 'completed' : 'not-completed'}
     >
       {text}
     </li>
+    </>
   )
 }
 
 const mapDispatchToProps = dispatch => ({
-  toggleToDo: id => dispatch( toggleToDo(id) )
+  toggleToDo: id => dispatch( toggleToDo(id) ),
+  removeToDo: id => dispatch( removeToDo(id) )
 })
 
 export default connect(null, mapDispatchToProps)(ToDo)
