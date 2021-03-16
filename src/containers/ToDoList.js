2ea94ac0027f8d593b776c750c390a1731c3fb21
rachelbuchta <>
2021-03-16 11:22:30 -0600

    Create ToDo component, map through todos and display

--- src/containers/ToDoList.js
+++ src/containers/ToDoList.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import ToDo from '../components/ToDo'
 
 const ToDoList = ({todos}) => {
-  console.log(todos)
+  const displayTodos = todos.map(todo => {
+    return (
+      <ToDo 
+        {...todo}
+        key={todo.id}
+      />
+    )
+  })
 
   return (
     <ul>
-
+      {displayTodos}
     </ul>
   )
 }
