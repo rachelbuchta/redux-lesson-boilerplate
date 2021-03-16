0814459958d6d3ac86766236035ea5ae59d10806
rachelbuchta <>
2021-03-16 11:13:13 -0600

    Connected form component to store, created new container of all todos

---
+++ src/containers/ToDoList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+const ToDoList = ({todos}) => {
+  console.log(todos)
+
+  return (
+    <ul>
+
+    </ul>
+  )
+}
+
+const mapStateToProps = state => ({
+  todos: state.todos
+})
+
+export default connect(mapStateToProps)(ToDoList)
